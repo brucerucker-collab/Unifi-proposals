2adb5cd34922eaa50cb543196c78df1fdda95989
brucerucker-collab <brucerucker@rucktech.net>
2026-04-18 12:05:41 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,35 @@
-export { default } from '../App.canonical';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useFirebaseAuth } from './useFirebaseAuth';
+import { LoginPage } from './LoginPage';
+import CanonicalApp from '../App.canonical';
+
+function ProtectedLayout() {
+  const { user, loading } = useFirebaseAuth();
+
+  if (loading) {
+    return <div style={{ padding: 24 }}>Loading…</div>;
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <CanonicalApp />;
+}
+
+export default function App() {
+  const { user, loading } = useFirebaseAuth();
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={<LoginPage loading={loading} isAuthed={!!user} />}
+        />
+        <Route path="/*" element={<ProtectedLayout />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
