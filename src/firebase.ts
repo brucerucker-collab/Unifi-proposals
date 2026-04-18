ea3f48be450694d45d1d815c87d4ed5fcd5287fd
brucerucker-collab <brucerucker@rucktech.net>
2026-04-18 12:39:27 -0500

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyA_88O0L3vJ35Z0_NkdvTMcgK7smWS94Qg",
+  authDomain: "unifi-proposals.firebaseapp.com",
+  projectId: "unifi-proposals",
+  storageBucket: "unifi-proposals.firebasestorage.app",
+  messagingSenderId: "1004598907196",
+  appId: "1:1004598907196:web:de1c2b9b6afb2b98bd8e39",
+  measurementId: "G-5D2DEHF0QH"
+};
 };
 
 const app = initializeApp(firebaseConfig);
